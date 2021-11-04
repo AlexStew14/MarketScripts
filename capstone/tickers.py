d1de4446b9ae8295f5617c7bc8815ff8c64546d3
alex <alexstew6050@gmail.com>
2021-11-04 15:19:15 -0600

    Updated notebooks

--- capstone/tickers.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-nasdaq_tickers = pd.read_csv('nasdaq_tickers.csv')
-nyse_tickers = pd.read_csv('nyse_tickers.csv')
-
-both_tickers = pd.concat([nasdaq_tickers, nyse_tickers])
-both_tickers.to_csv('tickers.csv', index=False)
